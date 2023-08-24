708025918f4c6540776e5604cc29bde7e074f587
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-08-24 14:52:54 -0300

    Adcionado arquivos inicias

---
+++ calculadora/src/main/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import './Calculadora.css'
+
+class Calculadora extends Component {
+  render() {
+    return <div className='calculadora'>
+        calc
+    </div>
+  }
+}
+
+/* export the component to be used in other components */
+export default Calculadora
