cfa1f1b405b9aef4a716123479beb7faada22c97
M319209 <rafael.anacleto@SL094>
2023-08-25 08:54:59 -0300

    Criado componente button

--- calculadora/src/main/Calculadora.jsx
+++ calculadora/src/main/Calculadora.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
 import './Calculadora.css'
+import { Button } from '../components/Button'
 
 class Calculadora extends Component {
   render() {
     return <div className='calculadora'>
-        calc
+        <Button/>
     </div>
   }
 }
