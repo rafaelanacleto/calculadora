e0f43e1cb4ec4ad291fb437ef5ba737775bcc208
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-08-25 15:48:20 -0300

    Criado display da calculadora

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import './Button.css';
+
 export const Button = (props) => {
   return (
     <div>
-        <button>0</button>
+      <button className={`
+        button operation
+      `}>
+        {props.label}
+      </button>
     </div>
-  )
-}
+    )
+  }
+  
